f18112b73ff1290e32342f373fa654c97148eb7b
Oliver Turpo <cliver20oliver23gmail.com>
2025-06-12 22:58:37 -0500

    cambiando mensaje de Prueba del Hola Mundo

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("¡Hola Mundo! - Sistema IESTA Colegio funcionando correctamente 🎓")
+    return HttpResponse("Para Servirle Mi King 👑")
 
 def status(request):
     import django
